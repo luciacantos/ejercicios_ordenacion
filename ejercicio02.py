21f0b46bfba8f9435e4bf4058a3ec2807a6267ad
luciacantos <luciacantos22@gmail.com>
2023-10-05 05:13:53 +0200

    ordenar pesos

---
+++ ejercicio02.py
@@ -0,0 +1,14 @@
+'''
+B. Prevención de riegos
+Hay n niños que quieren subir a una noria, y tu tarea es encontrar una góndola para cada
+niño.
+Cada góndola puede tener uno o dos niños, y además, el peso total en una góndola no
+puede exceder x. Conoces el peso de cada niño.
+¿Cuál es el número mínimo de góndolas necesarias para los niños?
+'''
+
+n, x = map(int, input().split())
+pesos = list(map(int, input().split()))
+
+pesos.sort()
+

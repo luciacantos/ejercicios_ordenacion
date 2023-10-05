e887ac0ddf89e0b1efbdb757f69c18c9d18b0c2a
luciacantos <luciacantos22@gmail.com>
2023-10-05 05:25:56 +0200

    nº peliculas y lista horarios

---
+++ ejercicio03.py
@@ -0,0 +1,11 @@
+'''
+C. SSIFF
+Se está dando lugar el Festival de Cine de San Sebastián. Tienes el abono completo y
+quieres ver tantas películas como puedas.
+Dispones de todos los horarios con la hora a la que empieza y acaba cada película.
+¿Cuántas películas puedes ver como máximo?
+'''
+
+n = int(input())
+horarios = []
+
